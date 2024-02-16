a799a7b4d22df7793d996fde2d4f7d84a3301c0a
yuhuiwang1228 <1670594368@qq.com>
2024-02-17 03:54:31 +0800

    organize

--- weiming/run.sh
+++ weiming/run.sh
@@ -3,8 +3,9 @@ pip install -r requirements.txt
 python ./Weiming/clean_txt.py -i ./Weiming/textbook_grade7_termA.txt -o ./Weiming/textbook_grade7_termA_clean.txt
 python ./Weiming/docx_to_txt.py --d ./Weiming/quality_grade7_termA.docx --t ./Weiming/quality_grade7_termA.txt
 
-cp ./Weiming/textbook_grade7_termA_clean.txt ./data/EN
-cp ./Weiming/quality_grade7_termA.txt ./data/EN/wiki_quality_extend.txt
+cp ./auto_phrase.sh ./weiming
+cp ./weiming/textbook_grade7_termA_clean.txt ./data/EN
+cp ./weiming/quality_grade7_termA.txt ./data/EN/wiki_quality_extend.txt
 cat ./data/EN/wiki_quality.txt >> ./data/EN/wiki_quality_extend.txt
 
 sudo docker run -v $PWD/models:/autophrase/models -it \
@@ -15,13 +16,16 @@ sudo docker run -v $PWD/models:/autophrase/models -it \
 sudo docker run -v $PWD/data:/autophrase/data -v $PWD/models:/autophrase/models -it \
     -e RAW_TRAIN=data/EN/DBLP.5K.txt \
     -e ENABLE_POS_TAGGING=1 \
-    -e MIN_SUP=30 -e THREAD=10 \
+    -e MIN_SUP=3 -e THREAD=10 \
     -e MODEL=models/DBLP5K \
-    -e TEXT_TO_SEG=data/EN/DBLP.5K.txt  \
     remenberl/autophrase
 
-sudo docker run -v $PWD/data:/autophrase/data -v $PWD/models:/autophrase/models -it \
-    -e RAW_TRAIN=data/EN/textbook_grade7_termA_clean.txt \
+sudo docker run \
+    -v $PWD/weiming:/autophrase/data \
+    -v $PWD/models:/autophrase/models \
+    -v $PWD/weiming:/autophrase/weiming -it \
+    -e DATA_DIR=data \
+    -e RAW_TRAIN=data/cache/textbook_grade7_termA_clean.txt \
     -e ENABLE_POS_TAGGING=1 \
     -e MIN_SUP=3 -e THREAD=10 \
     -e MODEL=models/MyModel7A \
